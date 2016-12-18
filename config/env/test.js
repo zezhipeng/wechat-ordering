5acbe65303371c54a60a9df59cb0b271762559d7
zezhipeng <372962347@qq.com>
2016-12-18 09:23:49 +0800

    save

--- config/env/test.js
+++ config/env/test.js
@@ -8,6 +8,6 @@ const client = new OAuth('wx33d67a99c493f926', 'fa0b9766fa75c7f7726f7382fb360b76
 
 module.exports = {
   client: client,
-  port: 2333,
+  port: 80,
   db: 'mongodb://localhost/noobjs_test'
 };
