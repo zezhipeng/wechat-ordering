b194ca7592eb07f38b0b36e4ac5bd1b33c2b7bf9
zezhipeng <372962347@qq.com>
2017-01-06 16:20:11 +0800

    add wechat api

--- components/user/components/account.vue
+++ components/user/components/account.vue
@@ -104,6 +104,24 @@ export default {
     $.get('/myOrder').then(res => {
       vm.$store.commit('myOrder', res)
     })
+    var sign = $.get(`/wx/signature?url=${window.location.href}`, res => {
+      wx.config({
+          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          appId: 'wx3c3c10b371693534', // 必填，公众号的唯一标识
+          timestamp: 12345, // 必填，生成签名的时间戳
+          nonceStr: 'WECHAT_API_SERVICE', // 必填，生成签名的随机串
+          signature: res,// 必填，签名，见附录1
+          jsApiList: ['chooseWXPay'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+      })
+
+      wx.ready(function(){
+          // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+      });
+      wx.error(function(res){
+          // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+      });
+    })
+
   },
   methods: {
     startIcon(i) {
