0e0b335c22cd0d3d84a0dbeafa3831336eb4eb5f
zezhipeng <372962347@qq.com>
2017-01-06 11:41:15 +0800

    save

--- config/env/test.js
+++ config/env/test.js
@@ -9,7 +9,7 @@ const client = new OAuth('wx3c3c10b371693534', 'e2bfdf4d83ff7a199cfea0bb052963e4
 module.exports = {
   client: client,
   port: 80,
-  db: 'mongodb://zezhipeng:123123@localhost'
+  db: 'mongodb://zezhipeng:123123@localhost/test'
 };
 
 
